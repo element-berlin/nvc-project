f4818c416cae0db48366d05ed8baf920fa5fb463
Marc Altmann <marcaltmann@posteo.de>
2017-02-14 23:21:34 +0100

    Change port

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 8080;
+
 app.get('/', function(req, res) {
   res.send('<h1>Hello World</h1>');
 });
 
-app.listen(3000);
+app.listen(port);
