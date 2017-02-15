ff86d36c665c1e3617aef90127beaae4e3bb0fc3
Marc Altmann <marcaltmann@posteo.de>
2017-02-15 12:54:10 +0100

    Change message

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello World</h1>');
+  res.send('<h1>Hello Element</h1>');
 });
 
 app.listen(port);
