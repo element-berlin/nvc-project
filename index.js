b6f05b0dc1c8725a0310cb169e8bd9470993d278
Marc Altmann <marcaltmann@posteo.de>
2017-02-16 22:58:26 +0100

    Add need post route

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const db = require('./db');
-
-let name = 'dummy';
-
-db().then(function() {
-  db.Name.create({
-    name: 'Clara',
-  }).then(function() {
-    db.Name.all().then(function(names) {
-      console.log(names);
-      name = names[0].name;
-    });
-  });
-});
-
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 const port = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res) {
-  res.render('index.ejs', {
-    name,
-  });
+  res.render('index.ejs');
+});
+
+app.post('/need', function(req, res) {
+  const need = {
+    name: req.body.name.trim(),
+    description: req.body.description.trim(),
+  };
+
+  console.log(need);
+
+  res.redirect('back');
 });
 
 app.listen(port);
