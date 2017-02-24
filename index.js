8d5c6f122f1d79ed992ded0b13e57f3343af94fd
Marc Altmann <marcaltmann@posteo.de>
2017-02-24 16:28:17 +0100

    Spell element name correctly

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
-  res.send('<h1>Hello Element</h1>');
+  res.send('<h1>Hello element</h1>');
 });
 
 app.listen(port);
